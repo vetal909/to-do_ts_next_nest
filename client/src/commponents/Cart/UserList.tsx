ee8a05523c6b0d38320f33a02ee8d28020d31864
vetal909 <vitaliekudrya@yandex.ru>
2023-10-09 12:16:27 +0300

    first done

---
+++ client/src/commponents/Cart/UserList.tsx
@@ -0,0 +1,17 @@
+import { IUser } from "../../types/types";
+import { FC } from "react";
+import UserItem from "../UserItem";
+
+interface UserListProps {
+  users: IUser[];
+}
+const UserList: FC<UserListProps> = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+export default UserList;
